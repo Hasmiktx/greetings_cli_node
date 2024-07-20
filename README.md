046b77277b6b9cf21021c560a9f3b676bd42c78a
Hasmiktx <hasmiktx@gmail.com>
2024-07-20 19:33:59 +0400

    added language

--- README.md
+++ README.md
@@ -7,10 +7,16 @@ A simple command-line tool to greet users.
 1. Clone the repository
 2. Navigate to the project directory
 3. Run `npm install` to install dependencies
-4.Example of expected result
+4. Example of expected result
   node index.js --name John --level 2
   Output:
    Hello, John! (Date and Time: 2024-01-01 12:34:56)
  node index.js -n Bob --greeting "Hey, welcome to Node js school" -lvl 1
   Output:
    Hey, welcome to Node js school Bob.
+5.  Language Support
+   Supported languages are English, Spanish, French, German, Italian
+   
+     Example of expected result
+    node index.js --language Spanish
+    Hola, guest
--- index.js
+++ index.js
@@ -2,11 +2,21 @@ import{program} from"commander";
 import chalk from "chalk";
 import dayjs from "dayjs";
 
+
+const greetings = {
+  English: 'Hello',
+  Spanish: 'Hola',
+  French: 'Bonjour',
+  German: 'Hallo',
+  Italian: 'Ciao',
+}
+
+
 program
   .version("1.0.0")
   .description("Node Greetings CLI")
   .option("-n, --name <type>", "Add your name","guest")
-  .option("-g, --greeting <type>", "Add your greeting","Hello")
+  .option("-g, --greeting <type>", "Add your greeting")
   .option("-lvl, --level <type>", "1 or 2","1")
   .option("-l, --language <type>", "Choose language","English")
 
@@ -14,12 +24,12 @@ program
 
   .action((options) => {
      const {name,greeting,level,language}=program.opts();
-
+      let greetingWithLanguage= greeting? greeting: greetings[language]
      if(level == 2){
         const time = dayjs().format('YYYY-MM-DD HH:mm:ss');
-        console.log(chalk.blue(`${greeting}, ${name}! ( Date and Time: ${time})`));
+        console.log(chalk.blue(`${greetingWithLanguage}, ${name}! ( Date and Time: ${time})`));
       }else{
-        console.log(chalk.yellow(`${greeting} ${name}!`))
+        console.log(chalk.yellow(`${greetingWithLanguage} ${name}!`))
       }
   });
 
